22f56730e04694e0b38a4ac7969629392fba168f
dawei <187533154+dawei1898@users.noreply.github.com>
2025-09-28 21:42:38 +0800

    feat: spring-ai-alibaba 升级为 1.0.0.3

--- src/main/java/com/alibaba/ai/demo/config/ToolCallAutoConfiguration.java
+++ src/main/java/com/alibaba/ai/demo/config/ToolCallAutoConfiguration.java
@@ -3,7 +3,7 @@ package com.alibaba.ai.demo.config;
 
 import com.alibaba.ai.demo.tools.TimeTools;
 import com.alibaba.ai.demo.tools.WeatherTools;
-import com.alibaba.cloud.ai.toolcalling.time.GetCurrentTimeByTimeZoneIdService;
+import com.alibaba.cloud.ai.toolcalling.time.GetTimeByZoneIdService;
 import com.alibaba.cloud.ai.toolcalling.weather.WeatherService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +13,11 @@ import org.springframework.context.annotation.Configuration;
  * 工具配置
  */
 @Configuration
-@ConditionalOnClass(GetCurrentTimeByTimeZoneIdService.class)
+@ConditionalOnClass(GetTimeByZoneIdService.class)
 public class ToolCallAutoConfiguration {
 
     @Bean
-    public TimeTools timeTools(GetCurrentTimeByTimeZoneIdService service) {
+    public TimeTools timeTools(GetTimeByZoneIdService service) {
         return new TimeTools(service);
     }
 
